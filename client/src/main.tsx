20f45e9684d4b48f274e1a846e32cbe50160d7cf
Karan Kumar Agarwal <darkside992901@gmail.com>
2026-05-23 23:28:52 +0530

    metadata

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
